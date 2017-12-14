240124b6212bb82e52466a7c30bdd45339691bec
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-14 21:12:45 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,13 +16,13 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-		:railroads =>{:pieces => 4, :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {}}, :rent_in_dollars => {:one_piece_owned => 100, :four_pieces_owned => 200}}
+		:railroads =>{:pieces => 4, :names => {}, :rent_in_dollars => {}}
 	}
 end
 
 def monopoly_with_third_tier
 	monopoly = {
-		:railroads =>{:pieces => 4, :names => {}, :rent_in_dollars => {}}
+		:railroads =>{:pieces => 4, :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {}}, :rent_in_dollars => {:one_piece_owned => 100, :four_pieces_owned => 200}}
 	}
 end
 
